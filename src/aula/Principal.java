a58e719f045e00ea618197b510c682d36d6c27a0
archanjojr <math.calazans56@hotmail.com>
2017-08-31 10:20:26 -0300

    termino da atividade 2

--- src/aula/Principal.java
+++ src/aula/Principal.java
@@ -34,33 +34,40 @@ public class Principal {
 
 	public static void criar() {
 		/* pedir para o usuário fornecer a quantidade de elementos da sequência */
+		String men;
+		men = JOptionPane.showInputDialog("Quantidade de elementos ");
 		/* checar se o usuário não clicou no botão Cancelar da janela */
-		/*
-		 * verifica se já existe uma sequência, neste caso deverá ser chamado o método
-		 * resize
-		 */
-		/*
-		 * caso a sequência não existe, então será necessário criar um objeto do tipo
-		 * Sequencia
-		 */
+		if(men != null){
+		    /* verifica se já existe uma sequência, neste caso deverá ser chamado o método resize*/
+		    if (sequencia !=null){
+		        sequencia.resize(Integer.parseInt(men));
+            }else{
+		    /* caso a sequência não existe, então será necessário criar um objeto do tipo Sequencia */
+		        sequencia = new Sequencia(Integer.parseInt(men));
+            }
+        }
+
 	}
 
 	public static void add() {
-		/*
-		 * se o objeto sequencia não existir, então deverá exibir uma mensagem para o
-		 * usuário
-		 */
-		/*
-		 * se o objeto sequencia existir, então deverá pedir para o usuário fornecer o
-		 * número a ser adicionado
-		 */
+
+
+		if(sequencia!=null){
+		    /* se o objeto sequencia existir, então deverá pedir para o usuário fornecer o número a ser adicionado */
+		    String men = JOptionPane.showInputDialog("DIGITE O NUMERO A SER ADICIONADO");
+        }else{
+		    /* se o objeto sequencia não existir, então deverá exibir uma mensagem para o  usuário */
+            JOptionPane.showMessageDialog(null,"Primeiro é necessário criar	a sequência usando a opção 1");
+        }
+
 	}
 
 	public static void imprimir() {
-		/* se o objeto sequencia não existir, então deverá exibir uma mensagem para o usuário */
+
 		if (sequencia != null) {
 			sequencia.imprimir();
 		}else{
+		    /* se o objeto sequencia não existir, então deverá exibir uma mensagem para o usuário */
 			JOptionPane.showMessageDialog(null,"Primeiro é necessário criar	a sequência usando a opção 1");
 		}
 	}
